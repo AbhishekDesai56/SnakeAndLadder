6bfa9d263368471b59b0ca628d401dc3c5bc41a9
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-05-30 15:20:12 +0530

    Single Player at Position One

--- Player.java
+++ Player.java
@@ -1,5 +1,12 @@
 package SnakeLadder;
 
 public class Player {
-
+	private String name;
+	private static Die die;
+	
+	public Player(String name) {
+		die = new Die();
+		this.name = name;
+	}
+	
 }
--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,18 @@
 package SnakeLadder;
 
-public class SnakeLadder {
 
+import java.util.*;
+
+public class SnakeLadder {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Snakes & Ladders");
+		
+		int numberOfPlayer = 1;
+		
+		List<Player> players = new ArrayList<Player>();
+		for (int index= 0; index < numberOfPlayer; index++) {
+			Player player = new Player("P" + index);
+			players.add(player);
+		}	
+	}
 }
